8e89f881d25f95c18e26b6f6c5a29d51ab0dccaa
AiaRup <aiale88@gmail.com>
2018-05-20 12:06:07 +0300

    complete functions

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
+let TennisGame = function (player1, player2) {
+  this.playerOne = player1;
+  this.playerSecond = player2;
+  this.playerOneScore = 0;
+  this.playerTwoScore = 0;
+};
 
+TennisGame.prototype.playerOneScores = function () {
+  this.playerOneScore++;
+};
+
+TennisGame.prototype.playerTwoScores = function () {
+  this.playerTwoScore++;
+};
+
+TennisGame.prototype.isDeuce = function () {
+  return (this.playerOneScore === this.playerTwoScore && this.playerOneScore >= 3 && this.playerTwoScore >= 3);
+};
+
+TennisGame.prototype.playerWithHighestScore = function () {
+  if (this.playerOneScore > this.playerTwoScore) {
+    return this.playerOne;
+  }
+  return this.playerSecond;
+};
+
+TennisGame.prototype.hasWinner = function () {
+  let playerOneLeads = (this.playerOneScore - this.playerTwoScore) >= 2;
+  let playerTwoLeads = (this.playerTwoScore - this.playerOneScore) >= 2;
+  return ((playerOneLeads && this.playerOneScore > 3) || (playerTwoLeads && this.playerTwoScore > 3));
+};
+
+TennisGame.prototype.hasAdvantage = function () {
+  let playerOneAdvantage = (this.playerOneScore - this.playerTwoScore) === 1;
+  let playerTwoAdvantage = (this.playerTwoScore - this.playerOneScore) === 1;
+  return ((playerOneAdvantage && this.playerTwoScore >= 3) || (playerTwoAdvantage && this.playerOneScore >= 3));
+};
+
+TennisGame.prototype.translateScore = function (score) {
+  return score === 0 ? 'Love' : (score === 1 ? 'Fifteen' : (score === 2 ? 'Thirty' : 'Forty'));
+};
+
+TennisGame.prototype.getScore = function () {
+  if (TennisGame.prototype.hasWinner()) {
+    return (TennisGame.prototype.playerWithHighestScore() + ' wins');
+  } else if (TennisGame.prototype.hasAdvantage()) {
+    return ('Advantage ' + TennisGame.prototype.playerWithHighestScore());
+  } else if (TennisGame.prototype.isDeuce()) {
+    return 'Deuce';
+  } else if (this.playerOneScore === this.playerTwoScore) {
+    return (TennisGame.prototype.translateScore(this.playerOneScore) + ' all');
+  } else {
+    return TennisGame.prototype.translateScore(this.playerOneScore) + ', ' + TennisGame.prototype.translateScore(this.playerTwoScore);
+  }
+};
--- spec/excerciseSpec.js
+++ spec/excerciseSpec.js
@@ -1,8 +1,8 @@
 var game;
 
 // creates a new game before each spec
-beforeEach(function () {
-  game = new TennisGame("Aaron" , "Avi"); 
+beforeEach(function() {
+  game = new TennisGame('Aaron', 'Avi');
 });
 
 // resets players scores after each spec
@@ -11,24 +11,24 @@ afterEach(function() {
   game.playerTwoScore = 0;
 });
 
-describe("playerOneScores", function () {
-  it("should increase player one's score by one", function () {
+describe('playerOneScores', function() {
+  it("should increase player one's score by one", function() {
     game.playerOneScores();
 
     expect(game.playerOneScore).toEqual(1);
   });
 });
 
-describe("playerTwoScores", function () {
-  it("should increase player twos's score by one", function () {
+describe('playerTwoScores', function() {
+  it("should increase player twos's score by one", function() {
     game.playerTwoScores();
 
     expect(game.playerTwoScore).toEqual(1);
   });
 });
 
-describe("isDeuce", function () {
-  it("should return true if the players have the same score that is 3 or above", function () {
+describe('isDeuce', function() {
+  it('should return true if the players have the same score that is 3 or above', function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -40,7 +40,7 @@ describe("isDeuce", function () {
     expect(game.isDeuce()).toEqual(true);
   });
 
-  it("should return false if the players don't have the same score but one is 3 or above", function () {
+  it("should return false if the players don't have the same score but one is 3 or above", function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -54,16 +54,16 @@ describe("isDeuce", function () {
   });
 });
 
-describe("playerWithHighestScore", function () {
-  it("should return the string of the player with the highest score", function () {
+describe('playerWithHighestScore', function() {
+  it('should return the string of the player with the highest score', function() {
     game.playerOneScores();
 
-    expect(game.playerWithHighestScore()).toEqual("Aaron");
+    expect(game.playerWithHighestScore()).toEqual('Aaron');
   });
 });
 
-describe("hasWinner", function () {
-  it("should return true if someone has won", function () {
+describe('hasWinner', function() {
+  it('should return true if someone has won', function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -72,13 +72,13 @@ describe("hasWinner", function () {
     expect(game.hasWinner()).toEqual(true);
   });
 
-  it("should return false if there is no winner", function () {
+  it('should return false if there is no winner', function() {
     expect(game.hasWinner()).toEqual(false);
   });
 });
 
-describe("hasAdvantage", function () {
-  it("should return true if a player has the advantage", function () {
+describe('hasAdvantage', function() {
+  it('should return true if a player has the advantage', function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -92,40 +92,40 @@ describe("hasAdvantage", function () {
     expect(game.hasAdvantage()).toEqual(true);
   });
 
-  it("should return false if a player does no players has the advantage", function () {
+  it('should return false if a player does no players has the advantage', function() {
     expect(game.hasAdvantage()).toEqual(false);
   });
 });
 
-describe("translateScore", function () {
-  it("should return 'Love' if the score is 0", function () {
-    expect(game.translateScore(0)).toEqual("Love");
+describe('translateScore', function() {
+  it("should return 'Love' if the score is 0", function() {
+    expect(game.translateScore(0)).toEqual('Love');
   });
 
-  it("should return 'Fifteen' if the score is 1", function () {
-    expect(game.translateScore(1)).toEqual("Fifteen");
+  it("should return 'Fifteen' if the score is 1", function() {
+    expect(game.translateScore(1)).toEqual('Fifteen');
   });
 
-  it("should return 'Thirty' if the score is 2", function () {
-    expect(game.translateScore(2)).toEqual("Thirty");
+  it("should return 'Thirty' if the score is 2", function() {
+    expect(game.translateScore(2)).toEqual('Thirty');
   });
 
-  it("should return 'Forty' if the score is 3", function () {
-    expect(game.translateScore(3)).toEqual("Forty");
+  it("should return 'Forty' if the score is 3", function() {
+    expect(game.translateScore(3)).toEqual('Forty');
   });
 });
 
-describe("getScore", function () {
-  it("should return the winner if there is one", function () {
+describe('getScore', function() {
+  it('should return the winner if there is one', function() {
     game.playerTwoScores();
     game.playerTwoScores();
     game.playerTwoScores();
     game.playerTwoScores();
 
-    expect(game.getScore()).toEqual("Avi wins");
+    expect(game.getScore()).toEqual('Avi wins');
   });
 
-  it("should return the advantage if there is one", function () {
+  it('should return the advantage if there is one', function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -137,10 +137,10 @@ describe("getScore", function () {
     game.playerTwoScores();
     game.playerTwoScores();
 
-    expect(game.getScore()).toEqual("Advantage Avi");
+    expect(game.getScore()).toEqual('Advantage Avi');
   });
 
-  it("should return the 'Deuce' if there is one", function () {
+  it("should return the 'Deuce' if there is one", function() {
     game.playerOneScores();
     game.playerOneScores();
     game.playerOneScores();
@@ -149,23 +149,23 @@ describe("getScore", function () {
     game.playerTwoScores();
     game.playerTwoScores();
 
-    expect(game.getScore()).toEqual("Deuce");
+    expect(game.getScore()).toEqual('Deuce');
   });
 
-  it("should return the 'all' if there is a tie", function () {
+  it("should return the 'all' if there is a tie", function() {
     game.playerOneScores();
 
     game.playerTwoScores();
 
-    expect(game.getScore()).toEqual("Fifteen all");
+    expect(game.getScore()).toEqual('Fifteen all');
   });
 
-  it("should return the score if there is no special case", function () {
+  it('should return the score if there is no special case', function() {
     game.playerOneScores();
 
     game.playerTwoScores();
     game.playerTwoScores();
 
-    expect(game.getScore()).toEqual("Fifteen, Thirty");
+    expect(game.getScore()).toEqual('Fifteen, Thirty');
   });
 });
